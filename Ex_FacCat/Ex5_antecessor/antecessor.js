2d6f03b45b6d6c2c83dfc9de275c24edbede0864
Lucca Macedo <89946241+MacedoMLucca@users.noreply.github.com>
2022-04-10 17:27:58 -0300

    Delete Ex_FacCat/Ex5_antecessor directory

--- Ex_FacCat/Ex5_antecessor/antecessor.js
+++
@@ -1,8 +0,0 @@
-function antecessor(){
-    num1=parseInt(prompt("Escreva um algoritmo para ler um valor e escrever o seu antecessor."));
-    
-    antecessor = num1 - 1;
-    
-    document.getElementById("resposta").innerHTML='O antecessor de '+num1+' é '+antecessor+'.';
-
-}
