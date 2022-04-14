f645a460761d65091a7870a4275135ebc70d5229
Lucca Macedo <89946241+MacedoMLucca@users.noreply.github.com>
2022-04-14 13:00:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// let contadora = 0;
+
+
+// while (contadora < 10) {
+//     alert(contadora);
+//     contadora++
+    
+// }
+
+// let contadoraDec = 10;
+
+
+// while (contadora >= 1) {
+//     alert(contadora);
+//     contadora--
+    
+// }
+
+let numero = 1;
+let contadora = 12;
+
+// numero = parseInt(prompt("Digite um numero :"));
+// while (contadora<11) {
+
+//     alert(numero + " * "+ contadora  + " = "+ numero*contadora )
+//  contadora++   
+// }
+
+// while (contadora <= 16) {
+//     while(numero <= 10){
+//         alert(numero)
+//         numero++
+//     } 
+
+//     alert(contadora)
+//     contadora++
+    
+// }
