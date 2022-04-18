5ac925e7d9493606b10616522e0ca04554aeda49
Lucca Macedo <89946241+MacedoMLucca@users.noreply.github.com>
2022-04-18 17:14:31 -0300

    Atualização de Atividades

---
+++ Ex_de_Aula/Operaçoes Matematicas/script.js
@@ -0,0 +1,29 @@
+
+function soma(){
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+
+    alert(num1 + num2)
+}
+function subt(){
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+
+    alert(num1 - num2)
+}
+function mult(){
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+
+    alert(num1 * num2)
+}
+function div(){
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+
+    alert(num1 / num2)
+}
+
+
+
+
