782e221285d146bff3e04e8cc163c356fd40e7f6
Lucca Macedo <89946241+MacedoMLucca@users.noreply.github.com>
2022-05-09 19:46:59 -0300

    Add files via upload

---
+++ Ex_Ap_Manzano/Loop_For/Ex1F_Para_Divisiveis_4_200/Divisiveis_4_200.js
@@ -0,0 +1,10 @@
+function Divisiveis_4_200(){
+
+    
+    for (let contadora = 1; contadora <= 200; contadora++) {
+     if (contadora % 4 == 0) {
+         alert(contadora)
+     }
+    }
+    
+}
---
+++ Ex_Ap_Manzano/Loop_For/Ex1G_Para_Elevado_a_3/Elevadoa3.js
@@ -0,0 +1,9 @@
+function Elevadoa3(){
+
+    
+    for (let contadora = 1; contadora <= 15; contadora++) {
+        let result = Math.pow(contadora,3)
+     alert(contadora + ' elevado a 3 é igual a: '+ result)
+    }
+    document.getElementById('resposta').innerHTML = contadora + ' elevado a 3 é igual a: '+ num;
+}
