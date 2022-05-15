7ed30edafe27b258be9250deb110ef17ca2f2866
Lucca Macedo <89946241+MacedoMLucca@users.noreply.github.com>
2022-05-15 19:17:56 -0300

    Delete Ex_Ap_Manzano/Manzano Ex6/Ex6_Converter_Graus directory

--- Ex_Ap_Manzano/Manzano Ex6/Ex6_Converter_Graus/Converter.js
+++
@@ -1,8 +0,0 @@
-function Converter(){
-    
-    let GCelsius , TempFinal;
-    GCelsius = parseInt(prompt('Escreva a temperatura que você converter: '));
-    TempFinal = (9*GCelsius+160)/5;
-
-    document.getElementById("resposta").innerHTML='Fazendo a conversao, ' + GCelsius + ' graus celsius sao equivalentes a '+ TempFinal+' fahrenheit';
-}
